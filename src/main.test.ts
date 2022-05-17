67462128c97025550ebc97ae2ce41fa8605ecabd
Dominik Mądro <d.madro@ideo.pl>
2022-05-17 20:17:17 +0200

    Test możliwości dodania pracownika do listy - red

--- src/main.test.ts
+++ src/main.test.ts
@@ -6,4 +6,20 @@ describe('employees', () => {
 
         expect(employees.list).toEqual([]);
     });
+
+    it('it is possible to add an employee', () => {
+        const employees = new Employees();
+
+        employees.add({
+            id: 1,
+            firstName: 'John',
+            lastName: 'Doe'
+        });
+
+        expect(employees.list).toEqual([{
+            id: 1,
+            firstName: 'John',
+            lastName: 'Doe'
+        }]);
+    });
 })
